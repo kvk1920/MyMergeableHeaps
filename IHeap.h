1389f0fb4d7c5dae9dab0f9bb663a853f2d545a8
kvk1920 <34111966+kvk1920@users.noreply.github.com>
2017-12-10 15:11:36 +0300

    Create IHeap.h

---
+++ IHeap.h
@@ -0,0 +1,18 @@
+//
+// Created by kvk on 12/10/17.
+//
+
+#ifndef HEAPS_IHEAP_H
+#define HEAPS_IHEAP_H
+
+class IHeap {
+public:
+    virtual int min() const = 0;
+    virtual void erase() = 0;
+    virtual void insert(int) = 0;
+    virtual void meld(IHeap &) = 0;
+    virtual bool empty() const = 0;
+    virtual void clear() = 0;
+};
+
+#endif //HEAPS_IHEAP_H
